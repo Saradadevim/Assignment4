67d77b71a1f22e5275c104eb0cc5d410d5498947
Saradadevim <saradakukkuu15@gmail.com>
2025-01-19 20:44:56 +0530

    Add files via upload

---
+++ Assignment4-2B.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment4_2B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Getting user input as  string 
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("The characters of the string in reverse order are:");
+
+            // loop to print characters in reverse order
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(input[i]);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment4-5E.cs
@@ -0,0 +1,46 @@
+﻿namespace Assignment4_5E
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //count a total number of alphabets, digits and special characters in a string.
+
+            // Getting user input as string 
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            // declaring variables 
+            int alphabets = 0;
+            int digits = 0;
+            int specialCharacters = 0;
+
+            // loops to iterate through each character in the string
+            foreach (char ch in input)
+            {
+                //loop to check alphabet, numbers or special characters in the string
+                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                {
+                    alphabets++; // Increment alphabet counter
+                }
+                else if (ch >= '0' && ch <= '9')
+                {
+                    digits++; // Increment digit counter
+                }
+                else
+                {
+                    specialCharacters++; // Increment special character counter
+                }
+            }
+
+            // Displaying the output of each the counts
+            Console.WriteLine($"Total Alphabets: {alphabets}");
+            Console.WriteLine($"Total Digits: {digits}");
+            Console.WriteLine($"Total Special Characters: {specialCharacters}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+
+        }
+    }
+}
